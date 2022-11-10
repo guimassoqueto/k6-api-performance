44cf698086609b07e2484b289b8ac5f9f357b363
guilherme.massoqueto <gui.massoqueto@gmail.com>
2022-11-10 10:06:30 -0300

    k6 Gauge

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 import http from "k6/http";
 import { check, group } from "k6";
-import { Rate } from "k6/metrics";
+import { Rate, Counter } from "k6/metrics";
 
 import { INVOICES_API_GET, INVOICES_API_POST } from "./urls.js";
 import invoicesPostBody from "./helpers/invoicesPostBody.js";
@@ -12,24 +12,30 @@ export const options = {
 };
 */
 const errorRate = new Rate('errorRate');
+const errorCount = new Counter('errorCount');
 
 export const options = {
   stages: [
     { duration: '10s', target: 10 },
-    { duration: '10s', target: 100 },
-    { duration: '20s', target: 1000 },
+    //{ duration: '10s', target: 100 },
+    //{ duration: '20s', target: 1000 },
   ],
   thresholds: {
     errorRate: [
-      { threshold: 'rate < 0.05', abortOnFail: true, delayAbortEval: '1s' },
+      { threshold: 'rate < 0.1', abortOnFail: true, delayAbortEval: '1s' },
     ],
+    errorCount: [
+      'count < 100',
+    ]
   },
 }
 
 export default function () {
     group('Invoices API post', function () {
         let res = http.post(INVOICES_API_POST, JSON.stringify(invoicesPostBody()));
-        check(res, { "status is 201": (r) => r.status === 201 });
+        let status_checked = check(res, { "status is 201": (r) => r.status === 201 });
         errorRate.add(res.status != 201);
+        if (!status_checked) errorCount.add(1);
     });
 }
+
